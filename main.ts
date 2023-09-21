dd152ba32a0d593c787f111df4dcd2538bef9f54
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-09-21 17:31:08 -0400

    Does math

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Sep 2023
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A = ' + (5 * 3).toString())
+basic.showString('P = ' + (2 *(5 + 3)))
